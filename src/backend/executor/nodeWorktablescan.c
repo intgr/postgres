b7402b57508a8987a5d12b3917aa079ba8096bdd
Marti Raudsepp <marti@juffo.org>
2011-12-12 20:17:14 +0200

    Refactor all callers of ExecInitExpr to take useCache argument

--- src/backend/executor/nodeWorktablescan.c
+++ src/backend/executor/nodeWorktablescan.c
@@ -160,10 +160,12 @@ ExecInitWorkTableScan(WorkTableScan *node, EState *estate, int eflags)
 	 */
 	scanstate->ss.ps.targetlist = (List *)
 		ExecInitExpr((Expr *) node->scan.plan.targetlist,
-					 (PlanState *) scanstate);
+					 (PlanState *) scanstate,
+					 true);
 	scanstate->ss.ps.qual = (List *)
 		ExecInitExpr((Expr *) node->scan.plan.qual,
-					 (PlanState *) scanstate);
+					 (PlanState *) scanstate,
+					 true);
 
 	/*
 	 * tuple table initialization
